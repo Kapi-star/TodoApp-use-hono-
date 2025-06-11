72070e17a9c2dce33239724facf11b8ce2ffb0e1
溝渕香菜 <mizobuchikana@Mac.elecom>
2025-06-11 10:02:23 +0900

    fix: change endpoint

--- app/api/[...route]/route.ts
+++ app/api/[...route]/route.ts
@@ -5,7 +5,7 @@ export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', (c) => {
+app.get('/todo', (c) => {
   return c.json({
     message: 'Hello from Hono!'
   })
